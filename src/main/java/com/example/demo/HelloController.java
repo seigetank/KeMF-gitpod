260c8f724a6bd1b3ef6d1ba6bb98a47fd6b769ae
DATTAM_SHIN sw <seigetankk@gmail.com>
2025-08-19 07:25:07 +0000

    spring boot hello

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,8 @@
 package com.example.demo;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-  @GetMapping("/")
-  public String hello() {
-    return "ok";
-  }
+  @GetMapping("/") public String hello() { return "ok"; }
 }
